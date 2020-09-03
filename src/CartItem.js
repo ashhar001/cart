20307e91ed52f4bb12fe774ccbefaca6f6d53dcc
ashhar001 <ashar.ansari1020@gmail.com>
2020-09-03 13:09:13 +0530

    Adding Inrement

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price: 999,
+            title: 'Phone',
+            qty: 1,
+            img: ''
+        }
+    }
+    increaseQty = () =>{
+        console.log('this', this.state);
+    }
     render(){
+        const{price, title, qty} = this.state;
         return(
             <div className="cart-item">
                 <div className="left-block">
@@ -9,12 +22,24 @@ class CartItem extends React.Component{
                 </div>
 
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs. 999</div>
-                    <div style={{ color:'#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>Rs. {price}</div>
+                    <div style={{ color:'#777'}}>Qty: {qty}</div>
 
                     <div className="car-item-actions">
                         {/*buttons*/}
+                        <img alt="increase" 
+                        className="action-icons" 
+                        src="https://image.flaticon.com/icons/svg/992/992651.svg"
+                        onClick={this.increaseQty}
+                        />
+                        <img alt="decrease" 
+                        className="action-icons" 
+                        src="https://image.flaticon.com/icons/svg/659/659892.svg"/>
+                        <img alt="delete" 
+                        className="action-icons" 
+                        src="https://image.flaticon.com/icons/svg/1345/1345823.svg"/>
+                        
                     </div>  
                 </div>
             </div>
